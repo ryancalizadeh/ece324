632c89e55bedb636fbf2e7c63eb70c4ccdee8f9b
Ryan Alizadeh <ryancalizadeh@gmail.com>
2024-04-09 22:05:50 -0400

    Update naming convention and add better saving mechanisms

--- ConfigGenerator.py
+++ ConfigGenerator.py
@@ -4,22 +4,25 @@ import json
 class ConfigGenerator:
     @classmethod
     def generate_config_files(cls, file_dir: str = "configs/"):
-        for sr_ratio in ConfigGenerator.sr_ratio_range():
-            for num_real_shots in ConfigGenerator.num_real_shots_range():
-                for ci_ratio in ConfigGenerator.ci_ratio_range():
-                    ConfigGenerator.save_config(sr_ratio, num_real_shots, ci_ratio, file_dir)
+        for i, ci_ratio in enumerate(ConfigGenerator.ci_ratio_range()):
+            for j, num_real_shots in enumerate(ConfigGenerator.num_real_shots_range()):
+                for k, sr_ratio in enumerate(ConfigGenerator.sr_ratio_range()):
+                    ConfigGenerator.save_config(ci_ratio, num_real_shots, sr_ratio, file_dir, i, j, k)
 
-    def save_config(sr_ratio, num_real_shots, ci_ratio, file_dir):
+    def save_config(ci_ratio, num_real_shots, sr_ratio, file_dir, ci_ratio_index, num_real_shots_index, sr_ratio_index):
         if not os.path.exists(file_dir):
             os.makedirs(file_dir)
 
-        name = f"sr{sr_ratio}_nrs{num_real_shots}_ci{ci_ratio}"
+        name = f"ci{ci_ratio}_nrs{num_real_shots}_sr{sr_ratio}"
 
         config = {
             "name": name,
-            "sr_ratio": sr_ratio,
+            "ci_ratio": ci_ratio,
             "num_real_shots": num_real_shots,
-            "ci_ratio": ci_ratio
+            "sr_ratio": sr_ratio,
+            "ci_ratio_index": ci_ratio_index,
+            "num_real_shots_index": num_real_shots_index,
+            "sr_ratio_index": sr_ratio_index
         }
 
         with open(os.path.join(file_dir, name), "w") as f:
@@ -27,14 +30,14 @@ class ConfigGenerator:
     
     @staticmethod
     def get_num_files():
-        return len(ConfigGenerator.sr_ratio_range()) * len(ConfigGenerator.num_real_shots_range()) * len(ConfigGenerator.ci_ratio_range())
+        return len(ConfigGenerator.ci_ratio_range()) * len(ConfigGenerator.num_real_shots_range()) * len(ConfigGenerator.sr_ratio_range())
 
     @staticmethod
     def get_results_shape():
-        return (len(ConfigGenerator.sr_ratio_range()), len(ConfigGenerator.num_real_shots_range()), len(ConfigGenerator.ci_ratio_range()))
+        return (len(ConfigGenerator.ci_ratio_range()), len(ConfigGenerator.num_real_shots_range()), len(ConfigGenerator.sr_ratio_range()))
 
     @staticmethod
-    def sr_ratio_range():
+    def ci_ratio_range():
         return [x/10 for x in range(1, 10, 2)]
 
     @staticmethod
@@ -42,5 +45,5 @@ class ConfigGenerator:
         return [2**i for i in range(1, 10, 2)]
 
     @staticmethod
-    def ci_ratio_range():
+    def sr_ratio_range():
         return [x/10 for x in range(1, 10, 2)]
--- ExperimentConfig.py
+++ ExperimentConfig.py
@@ -10,27 +10,42 @@ class ExperimentConfig:
     name: str
         name of the config
 
-    sr_ratio: float
-        ratio of synthetic to real data to use for training the test classifier
-    
+    ci_ratio: float
+        class imbalance ratio, ratio of the number of positive to negative samples in the real data
+
     num_real_shots: int
         number of real shots of training data to use
 
-    ci_ratio: float
-        class imbalance ratio, ratio of the number of positive to negative samples in the real data
+    sr_ratio: float
+        ratio of synthetic to real data to use for training the test classifier
+
+    ci_ratio_index: int
+        index of the class imbalance ratio in the range of class imbalance ratios
+
+    num_real_shots_index: int
+        index of the number of real shots in the range of number of real shots
+
+    sr_ratio_index: int
+        index of the synthetic to real ratio in the range of synthetic to real ratios
     """
 
     config_file: str
     name: str
-    sr_ratio: float
-    num_real_shots: int
     ci_ratio: float
+    num_real_shots: int
+    sr_ratio: float
+    ci_ratio_index: int
+    num_real_shots_index: int
+    sr_ratio_index: int
 
     def __init__(self, config_file: str):
         self.config_file = config_file
         with open(config_file, "r") as f:
             conf = json.load(f)
             self.name = conf["name"]
-            self.sr_ratio = conf["sr_ratio"]
-            self.num_real_shots = conf["num_real_shots"]
             self.ci_ratio = conf["ci_ratio"]
+            self.num_real_shots = conf["num_real_shots"]
+            self.sr_ratio = conf["sr_ratio"]
+            self.ci_ratio_index = conf["ci_ratio_index"]
+            self.num_real_shots_index = conf["num_real_shots_index"]
+            self.sr_ratio_index = conf["sr_ratio_index"]
