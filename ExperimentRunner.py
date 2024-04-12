e6fb626b5a7401f1ba37260a73c79d5344a85f4e
Sergey Noritsyn <124412084+SergeyNoritsyn@users.noreply.github.com>
2024-04-12 15:42:52 -0400

    Add training set for DCGAN

--- ExperimentRunner.py
+++ ExperimentRunner.py
@@ -18,7 +18,7 @@ class ExperimentRunner:
         self.dl = dl
 
     def run(self) -> Results:
-        train_x, train_y, test_x, test_y = self.dl.load_data()
+        train_gen, train_x, train_y, test_x, test_y = self.dl.load_data()
 
         num_imgs = self.config.num_real_shots
         s_ratio = self.config.sr_ratio
@@ -26,12 +26,12 @@ class ExperimentRunner:
         num_synth_imgs = int(num_imgs * s_ratio)
         
         generator_0 = Generator(self.config)
-        generator_0.train(train_x[0], train_y)
+        generator_0.train(train_gen[1], train_y)
         num_neg_imgs = int(num_synth_imgs * (1-class_imbalance))
         generated_neg = generator_0.generate(num_neg_imgs)
 
         generator_1 = Generator(self.config)
-        generator_1.train(train_x[1], train_y)
+        generator_1.train(train_gen[0], train_y)
         num_pos_imgs = num_synth_imgs - num_neg_imgs
         generated_pos = generator_1.generate()
 
