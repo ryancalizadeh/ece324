0ccdff93a45a3e9a3deeddd9ba43221f45cf6555
Sergey Noritsyn <124412084+SergeyNoritsyn@users.noreply.github.com>
2024-04-12 13:18:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-pip install medmnist
+from setuptools import setup, find_packages
+
+setup(
+    name='ece324',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'medmnist'
+    ],
+    url='https://github.com/ryancalizadeh/ece324',
+)
