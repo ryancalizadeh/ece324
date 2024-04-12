1485ec2472516ec121e4bd81992b6af417be16b4
Sergey Noritsyn <124412084+SergeyNoritsyn@users.noreply.github.com>
2024-04-12 13:54:23 -0400

    Removed bugs related to calling generator/descriminator functions

--- Generator.py
+++ Generator.py
@@ -8,8 +8,6 @@ class Generator:
 
     def __init__(self, config):
         self.config = config
-        self.generator = None
-        self.discriminator = None
         
     def dcgan_generator(self):
         model = tf.keras.Sequential()
@@ -28,7 +26,6 @@ class Generator:
         model.add(layers.LeakyReLU())
     
         model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-        print(model.output_shape)
         assert model.output_shape == (None, 28, 28, 1)
     
         return model
@@ -59,8 +56,6 @@ class Generator:
         return total_loss
         
     def train(self, x: np.ndarray, y: np.ndarray):
-        self.generator = dcgan_generator(config)
-        self.discriminator = dcgan_discriminator(config)
         """Train the generator based on the training data."""
         loss = tf.keras.losses.BinaryCrossentropy(from_logits=True) # maybe specify in config
         generator_optimizer = tf.keras.optimizers.Adam(1e-4)
@@ -81,10 +76,10 @@ class Generator:
                 noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
                 with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-                    generated_images = generator(noise, training=True)
+                    generated_images = dcgan_generator(noise, training=True)
                     
-                    real_output = discriminator(images, training=True)
-                    fake_output = discriminator(generated_images, training=True)
+                    real_output = dcgan_discriminator(images, training=True)
+                    fake_output = dcgan_discriminator(generated_images, training=True)
                     
                     gen_loss = generator_loss(fake_output, loss)
                     disc_loss = discriminator_loss(real_output, fake_output, loss)
@@ -108,5 +103,5 @@ class Generator:
         s_ratio = self.config["synth_ratio"]
         class_imbalance = self.config["ci_ratio"]
         seed = tf.random.normal([num_images, 100])
-        generated_images = generator(seed, training=False)
+        generated_images = dcgan_generator(seed, training=False)
         return generated_images
